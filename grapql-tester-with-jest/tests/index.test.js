e696a453a35e04228af360464f4890c36913d6f2
Chaitanya Korada <chaitanya.korada@hotmail.com>
2019-04-04 12:23:03 +0530

    Update index.test.js

--- grapql-tester-with-jest/tests/index.test.js
+++ grapql-tester-with-jest/tests/index.test.js
@@ -1,6 +1,8 @@
 const config = require('../src/config');
+const fixture = require('../tests/index-fixture.js');
 const tester = require('graphql-tester').tester;
 const chai = require('chai');
+const assert = require('chai');
 chaiHttp = require('chai-http');
 chai.use(chaiHttp);
 
@@ -10,10 +12,10 @@ describe('Performing Graphql queries in Article service', function () {
     try {
       res = await chai
         .request(`${config.APP_URL}`)
-        .post(`/auth/signin`)
+        .post(`/auth/v1/signin`)
         // .set('Authorization', 'Bearer ' + testUser.accessToken)
         .send({
-          username: "griesham",
+          username: "gri",
           password: "test1234"
         });
       expect(res.status).toBe(200);
@@ -24,7 +26,7 @@ describe('Performing Graphql queries in Article service', function () {
     }
   });
 
-  it('Get Article Data with Auth Token', done => {
+  it('Create Article Data with Auth Token', done => {
     const test = tester({
       url: `${config.APP_URL}/${config.GQL_URL_DIR}`,
       contentType: 'application/json',
@@ -32,41 +34,133 @@ describe('Performing Graphql queries in Article service', function () {
     });
     test(
       JSON.stringify({
-        query: `mutation{
-            createArticle(title:"hello user"){
-              id
-            }
-          }`,
+        query: fixture.createArticle,
 
       })
     )
       .then(res => {
         expect(res.status).toBe(200);
+        articleId = res.data.createArticle.id;
         done();
       })
       .catch(err => {
-        expect(err).toBe(null);
+        console.log(err);
         done();
       });
   });
 
-  it('Get Article Data without Auth Token', done => {
+  it('Create Article Data without Auth Token', done => {
+  
     const test = tester({
       url: `${config.APP_URL}/${config.GQL_URL_DIR}`,
       contentType: 'application/json',
     });
     test(
       JSON.stringify({
-        query: `mutation{
-            createArticle(title:"hello user"){
-              id
-            }
-          }`,
+        query: fixture.createArticle,
 
       })
     )
       .then(res => {
         expect(res.status).toBe(200);
+        expect(res.errors[0].message).toEqual("You must be signed in to view this resource.");
+        expect(res.errors[0].extensions.code).toEqual("UNAUTHENTICATED");
+        done();
+      })
+      .catch(err => {
+        console.log(err);
+        done();
+      });
+  });
+
+  it('Save Article Data with Auth Token', done => {
+  
+    const test = tester({
+      url: `${config.APP_URL}/${config.GQL_URL_DIR}`,
+      contentType: 'application/json',
+      authorization: `Bearer ${this.usertoken}`
+    });
+    test(
+      JSON.stringify({
+        query: fixture.saveArticle(articleId),
+
+      })
+    )
+      .then(res => {
+        expect(res.status).toBe(200);
+        expect(res.data.saveArticle.title).toEqual("this is my prognosis");
+        expect(res.data.saveArticle.id).toEqual(articleId);
+        done();
+      })
+      .catch(err => {
+        console.log(err);
+        done();
+      });
+  });
+  it('Save Article Data without Auth Token', done => {
+  
+    const test = tester({
+      url: `${config.APP_URL}/${config.GQL_URL_DIR}`,
+      contentType: 'application/json',
+    });
+    test(
+      JSON.stringify({
+        query: fixture.saveArticle(articleId),
+
+      })
+    )
+      .then(res => {
+        expect(res.status).toBe(200);
+        expect(res.errors[0].message).toEqual("You must be signed in to view this resource.");
+        expect(res.errors[0].extensions.code).toEqual("UNAUTHENTICATED");
+        done();
+      })
+      .catch(err => {
+        console.log(err);
+        done();
+      });
+  });
+
+  it('Publish Article Data with Auth Token', done => {
+  
+    const test = tester({
+      url: `${config.APP_URL}/${config.GQL_URL_DIR}`,
+      contentType: 'application/json',
+      authorization: `Bearer ${this.usertoken}`
+    });
+    test(
+      JSON.stringify({
+        query: fixture.publishArticle(articleId),
+
+      })
+    )
+      .then(res => {
+        expect(res.status).toBe(200);
+        expect(res.data.publishArticle.title).toEqual("this is my prognosis");
+        expect(res.data.publishArticle.id).toEqual(articleId);
+        done();
+      })
+      .catch(err => {
+        console.log(err);
+        done();
+      });
+  });
+  it('Publish Article Data without Auth Token', done => {
+  
+    const test = tester({
+      url: `${config.APP_URL}/${config.GQL_URL_DIR}`,
+      contentType: 'application/json',
+    });
+    test(
+      JSON.stringify({
+        query: fixture.publishArticle(articleId),
+
+      })
+    )
+      .then(res => {
+         expect(res.status).toBe(200);
+        expect(res.errors[0].message).toEqual("You must be signed in to view this resource.");
+        expect(res.errors[0].extensions.code).toEqual("UNAUTHENTICATED");
         done();
       })
       .catch(err => {
